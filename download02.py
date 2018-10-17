d3aa2a67ebfb5fa236f8175194b187287e768bb7
jeonsm1125 <jeonsm1125@naver.com>
2018-10-18 02:05:43 +0900

    Update download02.py

--- download02.py
+++ download02.py
@@ -5,7 +5,7 @@ filename = "book.mp4"
 ftp = ftplib.FTP()
 ftp.connect("tndusdl73.cafe24.com",21)
 ftp.login("tndusdl73","qlalf357")
-ftp.cwd("/www/tndus")
+ftp.cwd("/www/record")
 fd = open("/home/pi/" + filename,'wb')
 ftp.retrbinary("RETR " + filename, fd.write)
 fd.close()
