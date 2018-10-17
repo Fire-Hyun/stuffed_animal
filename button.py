5b2dcfbc96af8890bcc4b6c0e310cd9a04e42c9a
jeonsm1125 <jeonsm1125@naver.com>
2018-10-18 01:55:58 +0900

    Add files via upload

---
+++ button.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import pygame
+import RPi.GPIO as GPIO
+from time import sleep
+
+Button = 18
+
+GPIO.setmode (GPIO.BCM)  # GPIO 모드 세팅
+
+GPIO.setup(Button, GPIO.IN)  # 버튼 입력으로 설정
+
+pygame.init()
+
+try:
+        while True:
+                if GPIO.input(Button)==0:
+                    pygame.mixer.music.load("book.mp4")
+                    pygame.mixer.music.play()
+
+except KeyboardInterrupt:
+        GPIO.cleanup()
