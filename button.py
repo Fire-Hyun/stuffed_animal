75a8db2829b9b28a7fb2d97fd676585da4549820
jeonsm1125 <jeonsm1125@naver.com>
2018-10-20 00:46:55 +0900

    Update button.py

--- button.py
+++ button.py
@@ -11,11 +11,12 @@ GPIO.setmode (GPIO.BCM)  # GPIO 모드 세팅
 GPIO.setup(Button, GPIO.IN)  # 버튼 입력으로 설정
 
 pygame.init()
+pygame.mixer.init()
 
 try:
         while True:
                 if GPIO.input(Button)==0:
-                    pygame.mixer.music.load("book.mp4")
+                    pygame.mixer.music.load("book.wav")
                     pygame.mixer.music.play()
 
 except KeyboardInterrupt:
