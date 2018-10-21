7878ec4546e3e01f3a41025f8e7c90f5469426d2
jeonsm1125 <jeonsm1125@naver.com>
2018-10-21 16:53:20 +0900

    Update camera02.py

--- camera02.py
+++ camera02.py
@@ -1,14 +1,15 @@
-import picamera
+from picamera import PiCamera
+from time import sleep
 import time
 import datetime
 import os
 
 camera = picamera.PICamera()
 camera.start_preview()
-saveFileName = datetime.datetime.now().strftime('%y%m%d-%H%M%S%f')+'.h264'
-camera.start_recording('/home/pi/saveFileName')
+#saveFileName = datetime.datetime.now().strftime('%y%m%d-%H%M%S%f')+'.h264'
+camera.start_recording('/home/pi/2018_10_29.h264')
 time.sleep(10)
 camera.stop_recording()
 camera.stop_preview()
-os.system("MP4Box -add saveFileName.h264 saveFileName.mp4")
+os.system("MP4Box -add 2018_10_29.h264 2018_10_29.mp4")
 
